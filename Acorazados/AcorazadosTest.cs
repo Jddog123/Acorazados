f97ea29d9666c90647130fbcc56f3077c5b7ea8a
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-18 12:50:19 -0500

    Si_AgregoUnJugadorConUnDestructorConDosCoordenadasDebe_ArrojarExcepcion

--- Acorazados/AcorazadosTest.cs
+++ Acorazados/AcorazadosTest.cs
@@ -140,6 +140,9 @@ public class Juego
 
     public void AgregarJugador(List<(List<(int x, int y)> coordenadas, TipoBarco tipoBarco)> listaBarcos)
     {
+        if(listaBarcos.Any(barco => barco.tipoBarco == TipoBarco.Destructor && barco.coordenadas.Count() < 3))
+            throw new ArgumentException("El tipo de barco cañonero es de una coordenada");
+        
         if(listaBarcos.Any(barco => barco.tipoBarco == TipoBarco.Canonero && barco.coordenadas.Count() != 1))
             throw new ArgumentException("El tipo de barco cañonero es de una coordenada");
         
