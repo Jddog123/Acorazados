e03c6499a5d088320ab3639b4a7646f9dabf1777
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-18 12:06:41 -0500

    🟢Si_AgregoUnJugadorConUnCañoneroEnLaPosicion10_0_Debe_ArrojarExcepcion

--- Acorazados/AcorazadosTest.cs
+++ Acorazados/AcorazadosTest.cs
@@ -50,6 +50,11 @@ public class Juego
 
     public void AgregarJugador(List<(List<(int x, int y)> coordenadas, TipoBarco)> listaBarcos)
     {
+        if (listaBarcos[0].coordenadas[0].x > LimiteSuperiorPlataforma || listaBarcos[0].coordenadas[0].y < LimiteInferiorPlataforma)
+        {
+            throw new ArgumentException("Barco fuera del limite de la plataforma");
+        }
+        
         if (listaBarcos[0].coordenadas[0].x < LimiteInferiorPlataforma || listaBarcos[0].coordenadas[0].y > LimiteSuperiorPlataforma)
         {
             throw new ArgumentException("Barco fuera del limite de la plataforma");
