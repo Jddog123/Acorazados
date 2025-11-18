278eba90d31679061a1bbc5d252703cdb682d9c1
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-18 12:55:12 -0500

    🟢 Si_AgregoUnJugadorConUnDestructorConCuatroCoordenadasDebe_ArrojarExcepcion

--- Acorazados/AcorazadosTest.cs
+++ Acorazados/AcorazadosTest.cs
@@ -160,7 +160,7 @@ public class Juego
 
     public void AgregarJugador(List<(List<(int x, int y)> coordenadas, TipoBarco tipoBarco)> listaBarcos)
     {
-        if (listaBarcos.Any(barco => barco.tipoBarco == TipoBarco.Destructor && barco.coordenadas.Count() < 3))
+        if (listaBarcos.Any(barco => barco.tipoBarco == TipoBarco.Destructor && barco.coordenadas.Count() != 3))
             throw new ArgumentException("El tipo de barco destructor es de una coordenada");
 
         if (listaBarcos.Any(barco => barco.tipoBarco == TipoBarco.Canonero && barco.coordenadas.Count() != 1))
