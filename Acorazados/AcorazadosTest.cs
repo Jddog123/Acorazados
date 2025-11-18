438843811f7350e7f44e07468dbb0b7ecd8a7cd5
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-18 12:46:39 -0500

    🔵 Si_AgregoUnJugadorConUnCanoneroSinCoordenadasDebe_ArrojarExcepcion

--- Acorazados/AcorazadosTest.cs
+++ Acorazados/AcorazadosTest.cs
@@ -9,9 +9,8 @@ public class AcorazadosTest
     public void Si_AgregoUnJugadorConUnCañoneroEnLaPosicion0_10_Debe_ArrojarExcepcion()
     {
         var juego = new Juego();
-        List<(List<(int x, int y)> coordenadas, TipoBarco)> listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
-        List<(int x, int y)> coordenadasPrimerCañonero = new List<(int x, int y)>();
-        coordenadasPrimerCañonero.Add((0,10));
+        var listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
+        var coordenadasPrimerCañonero = new List<(int x, int y)> { (0,10) };
         listaBarcos.Add((coordenadasPrimerCañonero,TipoBarco.Canonero));
         
         Action result = () => juego.AgregarJugador(listaBarcos);
@@ -22,9 +21,8 @@ public class AcorazadosTest
     public void Si_AgregoUnJugadorConUnCañoneroEnLaPosicion10_0_Debe_ArrojarExcepcion()
     {
         var juego = new Juego();
-        List<(List<(int x, int y)> coordenadas, TipoBarco)> listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
-        List<(int x, int y)> coordenadasPrimerCañonero = new List<(int x, int y)>();
-        coordenadasPrimerCañonero.Add((10,0));
+        var listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
+        var coordenadasPrimerCañonero = new List<(int x, int y)> { (10,0) };
         listaBarcos.Add((coordenadasPrimerCañonero,TipoBarco.Canonero));
         
         Action result = () => juego.AgregarJugador(listaBarcos);
@@ -36,11 +34,13 @@ public class AcorazadosTest
     public void Si_AgregoUnJugadorConUnDestructorEnLaPosicion0_8_0_9_0_10Debe_ArrojarExcepcion()
     {
         var juego = new Juego();
-        List<(List<(int x, int y)> coordenadas, TipoBarco)> listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
-        List<(int x, int y)> destructor = new List<(int x, int y)>();
-        destructor.Add((0,8));
-        destructor.Add((0,9));
-        destructor.Add((0,10));
+        var listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
+        var destructor = new List<(int x, int y)>
+        {
+            (0, 8),
+            (0, 9),
+            (0,10)
+        };
         listaBarcos.Add((destructor,TipoBarco.Destructor));
         
         Action result = () => juego.AgregarJugador(listaBarcos);
@@ -78,10 +78,12 @@ public class AcorazadosTest
     public void Si_AgregoUnJugadorConUnCanoneroEnDosCoordenadasDebe_ArrojarExcepcion()
     {
         var juego = new Juego();
-        List<(List<(int x, int y)> coordenadas, TipoBarco)> listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
-        List<(int x, int y)> canonero = new List<(int x, int y)>();
-        canonero.Add((0,8));
-        canonero.Add((0,9));
+        var listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
+        var canonero = new List<(int x, int y)>
+        {
+            (0, 8),
+            (0, 9)
+        };
         listaBarcos.Add((canonero,TipoBarco.Canonero));
         
         Action result = () => juego.AgregarJugador(listaBarcos);
@@ -93,8 +95,8 @@ public class AcorazadosTest
     public void Si_AgregoUnJugadorConUnCanoneroSinCoordenadasDebe_ArrojarExcepcion()
     {
         var juego = new Juego();
-        List<(List<(int x, int y)> coordenadas, TipoBarco)> listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
-        List<(int x, int y)> canonero = new List<(int x, int y)>();
+        var listaBarcos = new List<(List<(int x, int y)> coordenadas, TipoBarco)>();
+        var canonero = new List<(int x, int y)>();
         listaBarcos.Add((canonero,TipoBarco.Canonero));
         
         Action result = () => juego.AgregarJugador(listaBarcos);
