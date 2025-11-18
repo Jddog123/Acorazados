1ae580a326f25bfd9a02e701e03f5442a754019b
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-18 10:23:24 -0500

    🔴Si_SeCreaElJuegoLasCoordenadas0_0y9_9_Debe_Existir

--- Acorazados/AcorazadosTest.cs
+++ Acorazados/AcorazadosTest.cs
@@ -1,9 +1,23 @@
-﻿namespace Acorazados;
+﻿using FluentAssertions;
+
+namespace Acorazados;
 
 public class AcorazadosTest
 {
     [Fact]
-    public void Test1()
+    public void Si_SeCreaElJuegoLasCoordenadas0_0y9_9_Debe_Existir()
     {
+        var juego = new Juego();
+
+        juego.ExisteCoordenada(ejeX: 0, ejeY: 0).Should().BeTrue();
+        juego.ExisteCoordenada(ejeX: 9, ejeY: 9).Should().BeTrue();
+    }
+}
+
+public class Juego
+{
+    public bool ExisteCoordenada(int ejeX, int ejeY)
+    {
+        throw new NotImplementedException();
     }
 }
